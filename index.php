07b600d4f8827cc27a51f2748163e999bee3af44
omegrex <omegrex@gmail.com>
2024-10-12 18:58:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login222</title>
+    <title>Login</title>
 </head>
 <body>
         <?php
